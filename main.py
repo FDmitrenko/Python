7e2a68d3545c279c3b74d3ec1ebf5e79e863973b
Fedor <theodorprogramming@gmail.com>
2025-01-26 17:16:47 +0300

    Создание базы данных

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sqlite3
 from aiogram import Bot, Dispatcher, types, executor
 
+con = sqlite3.connect("main.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE user_data(id, name, age)")
+res = cur.execute("SELECT * FROM user_data")
 API_TOKEN = '7857492751:AAGsD_ivlivrkalri1l4T9kqf-1vgIiiqC8'
 
 bot = Bot(token=API_TOKEN)
